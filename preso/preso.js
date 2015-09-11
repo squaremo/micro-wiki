63ffb73f64a5e21174bce33f90f5f6445aebad8b
Michael Bridgen <mikeb@squaremobius.net>
2015-09-11 11:21:22 +0200

    Minimal wikiness
    Now you can visit a page and enter content for it.

--- preso/preso.js
+++ preso/preso.js
@@ -1,9 +1,54 @@
 var http = require('http');
+var urlparse = require('url').parse;
+var qsparse = require('querystring').parse;
 
 var srv = http.createServer(handle);
-srv.listen(os.ENV['HTTP_PORT']);
+srv.listen(process.env['HTTP_PORT']);
+
+var pages = {};
 
 function handle(req, res) {
-    res.writeHeader();
+  var parts = urlparse(req.url);
+  var name = parts.pathname.substr(1);
+
+  if (req.method == "GET") {
+    handleGet(name, req, res);
+  }
+  else if (req.method == "POST") {
+    handlePost(name, req, res);
+  }
+}
+
+function handleGet(name, req, res) {
+  var content = pages[name];
+  if (content !== undefined) {
+    res.writeHeader(200);
+    writePage(res, name, content);
+  }
+  else {
+    res.writeHeader(404);
+    writePage(res, 'Not found sorry', '');
+  }
+}
+
+function handlePost(name, req, res) {
+  readAll(req, function(buf) {
+    var form = qsparse(buf.toString('utf8'));
+    pages[name] = form.content;
+    res.writeHeader(303, {'Location': '/'+name});
     res.end();
+  });
+}
+
+function writePage(res, name, content) {
+  res.write('<h1>' + name + '</h1>');
+  res.write(content, 'utf8');
+  res.write('<form method="POST"><textarea name="content"></textarea><input type="submit"/></form>');
+  res.end();
+}
+
+function readAll(s, k) {
+  var bufs = [];
+  s.on('data', function(b) { bufs.push(b); });
+  s.on('end', function() { k(Buffer.concat(bufs)); });
 }
